d9d1200261a2d91acce4bfa518f38336fbd7fb63
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-26 02:12:41 +0000

    Code works!

--- js/script.js
+++ js/script.js
@@ -9,56 +9,61 @@
 // Function that executes the user's food order
 function WaffleOrder() {
 
-  // Determining a constant for tax rate
+// Determining constants for tax rate, my toppings price, fried chicken prices and breakfast drinks price.
   const HST = 0.13;
+  const PRICE_WAFFLE_TOPPINGS = 2.00;
+  const PRICE_FRIED_CHICKEN_TENDERS = 2.00;
+  const PRICE_BREAKFAST_DRINKS = 1.00;
   
-  
-  // Initializing variables
+  // Initializing variable for base cost of waffles, which is determined depending on the size of waffle ordered.
   let baseWaffleCost = 0;
 
-  // Declaring variables for base waffle and optional drink
-  let waffleSmall = document.getElementById("small").value;
-  let waffleMedium = document.getElementById("medium").value;
-  let waffleLarge = document.getElementById("large").value;
-  let amountOfDrinks = parseInt(document.getElementById("drinks-amount").value);
+  // Getting user input for waffle size, number of fried chicken tenders and number of breakfast drinks.
+  let waffleSize = document.getElementById("size").value;
+  let numberOfFriedChickenTenders = parseInt(document.getElementById("fried-chicken-tenders").value);
+  let numberOfBreakfastDrinks = parseInt(document.getElementById("amount-drinks").value);
+  
+  // Establishing the number of waffle toppings selected given the number of checkboxes selected.
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
 
-  // Determining the cost of the base waffle using if/else statements
-
-  // If the waffle size is small
-  if (waffleSmall == "3.00") {
+  // Using determined value to declare variable for number of toppings chosen
+  let numberOfWaffleToppings = checkboxes.length;
+  
+  // Determining the cost of the toppings using number of toppings variable and cost of toppings constant
+  let costOfToppings = numberOfWaffleToppings * PRICE_WAFFLE_TOPPINGS;
+  
+  // Establishing the base cost of a waffle using compound if statements.
+  
+  // If the waffle size is small, then the user will owe $3.00.
+  if (waffleSize == "small") {
     baseWaffleCost = 3.00;
   }
-
-  // If the waffle size is medium
-  else if (waffleMedium == "4.00") {
+    
+  // Otherwise, if the waffle size is medium, then the user will owe $4.00.
+  else if (waffleSize == "medium") {
     baseWaffleCost = 4.00;
   }
-
-  // If the waffle size is large
-  else if (waffleLarge == "5.00") {
+    
+  // Otherwise, if the waffle size is large, then the user will owe $5.00.
+  else if (waffleSize == "large") {
     baseWaffleCost = 5.00;
+  }
+
+  // Else, the base cost for a waffle is $0.00.
+  else {
+    baseWaffleCost = 0.00;
+  }
   
-  //Else the waffle order will be 0 dollars
-    else  baseWaffleCost= 0.00;
+  // Establishing the cost of the optional sides, from the number of sides variables and the prices of sides constants.
+  let costOfFriedChicken = numberOfFriedChickenTenders * PRICE_FRIED_CHICKEN_TENDERS;
+  let costOfBreakfastDrinks = numberOfBreakfastDrinks * PRICE_BREAKFAST_DRINKS;
+  
+  // Calculating the user's subtotal, tax and total for their order
+  let subtotal = baseWaffleCost + costOfToppings + costOfFriedChicken + costOfBreakfastDrinks;
+  let taxes = subtotal * HST;
+  let total = subtotal + taxes;
 
-
-  // Determining the number of checkboxes selected
-  let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  let numberOfToppings = checkboxes.length;
-
-  // Determining constants of the toppings and using a variable to calculate the price of the toppings chosen
-  const UNIT_PRICE_TOPPINGS = 2.00;
-  let costOfToppings = numberOfToppings * UNIT_PRICE_TOPPINGS;
-
-  // Determining the constant for the drinks and using a variable to calculate the price of the drinks chosen
-  const UNIT_PRICE_DRINKS = 1.00;
-  let costOfDrinks = amountOfDrinks * UNIT_PRICE_DRINKS;
-
-  // Calculating the subtotal of the user (tax and total)
-  let subtotal = baseWaffleCost + costOfToppings + costOfDrinks;
-  let tax = subtotal * HST;
-  let total = subtotal + tax;
-
-  // Displaying the total price back to the user
-  document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + "." + " Thanks for your purchase!"; 
+  // Displaying the total to the user in the "results" div
+  document.getElementById("results").innerHTML = "You order is a " + waffleSize + " waffle. " + "For your waffle, you chose " + numberOfWaffleToppings + " toppings. You also decided to add " + numberOfFriedChickenTenders + " fried chicken tender(s) to your order, along with " + numberOfBreakfastDrinks + " beverage(s). Your subtotal is $" + subtotal.toFixed(2) + ". The calculated amount of taxes added due to the HST rate is $" + taxes.toFixed(2) + ". Which makes your total $" + total.toFixed(2) + ". Thanks for your purchase!";
+  
 }
